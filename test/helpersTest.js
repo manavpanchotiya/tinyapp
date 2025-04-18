5f7846f1fc8e09eef292d9c94cf0f13cc28f2098
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-18 00:23:16 -0500

    added additional test cases

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,7 @@
 const { assert } = require('chai');
 
 const { getUserByEmail } = require('../helpers');
+const { urlsForUser } = require('../express_server');
 
 const testUsers = {
   "userRandomID": {
@@ -15,6 +16,21 @@ const testUsers = {
   }
 };
 
+const sampleUrlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "user1"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userId: "user2"
+  },
+  "abc123": {
+    longURL: "http://www.example.com",
+    userId: "user1"
+  }
+};
+
 describe('getUserByEmail', function() {
   it("should return a user object when provided with an email that exists in the database", function() { 
     const user = getUserByEmail("user@example.com", testUsers)
@@ -26,5 +42,38 @@ describe('getUserByEmail', function() {
   it("should return undefined when provided with an email that doesn't exist in the database", function() {
     const user = getUserByEmail("noemail@example.com", testUsers)
     assert.strictEqual(user, undefined, "Should return undefined for non-existent email");
+  });
+});
+
+describe('urlsForUser', function() {
+  it('should return urls that belong to the specified user', function() {
+    const result = urlsForUser("user1", sampleUrlDatabase);
+    const expected = {
+      "b2xVn2": {
+        longURL: "http://www.lighthouselabs.ca",
+        userId: "user1"
+      },
+      "abc123": {
+        longURL: "http://www.example.com",
+        userId: "user1"
+      }
+    };
+    assert.deepEqual(result, expected);
+  });
+
+  it('should return an empty object if no urls belong to the specified user', function() {
+    const result = urlsForUser("user3", sampleUrlDatabase);
+    assert.deepEqual(result, {});
+  });
+
+  it('should return an empty object if the urlDatabase is empty', function() {
+    const result = urlsForUser("user1", {});
+    assert.deepEqual(result, {});
+  });
+
+  it('should not return any urls that do not belong to the specified user', function() {
+    const result = urlsForUser("user2", sampleUrlDatabase);
+    const notExpectedKey = "abc123";
+    assert.notProperty(result, notExpectedKey);
   });
 });
